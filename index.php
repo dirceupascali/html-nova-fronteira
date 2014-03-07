1ac433e27ef363ae7efdeb70a534639978d339b6
dirceupascali <dddirceu@gmail.com>
2014-03-07 10:18:10 -0300

    ajustando estrutura html - HOME

--- index.php
+++ index.php
@@ -3,73 +3,113 @@
 	<!-- Main Body -->
 	<section id="main-content">
 		<div id="home" class="center">
-			<h2>CONTEUDO</h2>
-			<div class="pics1">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics1"></div>
-	        <div class="pics2">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics2">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics3">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics1">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics2">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics3">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics1">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics2">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics3">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics1">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics2">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
-	        <div class="pics3">
-	            <img src="http://malsup.github.com/images/beach1.jpg">
-	            <img src="http://malsup.github.com/images/beach2.jpg">
-	            <img src="http://malsup.github.com/images/beach3.jpg">
-	        </div>
+			<section class="separate">
+				<div class="pics1 green"></div>
+				<div class="pics1"></div>
+				<div class="pics2">
+		            <div>
+		            	<h2>Lorem Ipsum</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem Ipsum 2</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem Ipsum 3</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		        </div>
+		    </section>
+		    <section class="separate">
+		    	<div class="pics3">
+		            <img src="http://malsup.github.com/images/beach1.jpg">
+		            <img src="http://malsup.github.com/images/beach2.jpg">
+		            <img src="http://malsup.github.com/images/beach3.jpg">
+		        </div>
+		    </section>
+		    <section class="separate">
+		    	<div class="pics1">
+		    		<div>
+		            	<h2>Lorem</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem 2</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem 3</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		    	</div>
+		    	<div class="pics1">
+		            <img src="http://malsup.github.com/images/beach1.jpg">
+		            <img src="http://malsup.github.com/images/beach2.jpg">
+		            <img src="http://malsup.github.com/images/beach3.jpg">
+		        </div>
+		        <div class="pics1 yellow"></div>
+		        <div class="pics1"></div>
+		    </section>
+
+		    <section class="separate">
+		    	<div class="pics2">
+		            <img src="http://malsup.github.com/images/beach1.jpg">
+		            <img src="http://malsup.github.com/images/beach2.jpg">
+		            <img src="http://malsup.github.com/images/beach3.jpg">
+		        </div>
+		        <div class="pics1 yellow">
+		        	<div>
+		            	<h2>Lorem</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem 2</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem 3</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		        </div>
+		    	<div class="pics1"></div>
+		    </section>
+	        <section class="separate">
+		        <div class="pics1"></div>
+		    	<div class="pics1">
+		            <img src="http://malsup.github.com/images/beach1.jpg">
+		            <img src="http://malsup.github.com/images/beach2.jpg">
+		            <img src="http://malsup.github.com/images/beach3.jpg">
+		        </div>
+		        <div class="pics1">
+		            <img src="http://malsup.github.com/images/beach1.jpg">
+		            <img src="http://malsup.github.com/images/beach2.jpg">
+		            <img src="http://malsup.github.com/images/beach3.jpg">
+		        </div>
+		        <div class="pics1"></div>
+		    </section>
+		    <section class="separate">
+		    	<div class="pics1">
+		            <img src="http://malsup.github.com/images/beach1.jpg">
+		            <img src="http://malsup.github.com/images/beach2.jpg">
+		            <img src="http://malsup.github.com/images/beach3.jpg">
+		        </div>
+		        <div class="pics1 green"></div>
+		        <div class="pics2 orange">
+		            <div>
+		            	<h2>Lorem Ipsum</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem Ipsum 2</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		            <div>
+		            	<h2>Lorem Ipsum 3</h2>
+		            	<p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>
+		            </div>
+		        </div>
+		    </section>
 		</div>
 	</section>
 
