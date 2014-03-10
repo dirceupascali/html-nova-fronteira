87cdfea74343358c41803dc452ab3fe6f095cd6f
LuigiLazzari <luigi.lc89@gmail.com>
2014-03-10 17:30:11 -0300

    publicacoes

---
+++ publicacoes.php
@@ -0,0 +1,38 @@
+<?php include('includes/header.php'); ?>
+
+	<!-- Main Body -->
+	<section id="main-content">
+		<?php include('includes/breadcrumb.php'); ?>
+		<div id="box-content" class="center">
+			<div class="tit-principal">
+				<h1>
+					<span class="sprite"></span>
+					Publicações
+				</h1>
+			</div>
+			<section id="publi-row">
+				<ul>
+					<li><img src="images/capa-revista.jpg" width="160px" height="220px">
+						<h3>Nossas Notícias</h3><p class="text"><span> # 33</span> Jan | Junho 2014</p>
+					</li>
+					<li><img src="images/capa-revista.jpg" width="160px" height="220px">
+						<h3>Nossas Notícias</h3><p class="text"><span> # 34</span> ago | Junho 2014</p>
+					</li>
+					<li><img src="images/capa-revista.jpg" width="160px" height="220px">
+						<h3>Nossas Notícias</h3><p class="text"><span> # 35</span> dez 2013</p>
+					</li>
+					<li><img src="images/capa-revista.jpg" width="160px" height="220px">
+						<h3>Nossas Notícias</h3><p class="text"><span> # 33</span> Jan | Junho 2014</p>
+					</li>
+					<li><img src="images/capa-revista.jpg" width="160px" height="220px">
+						<h3>Nossas Notícias</h3><p class="text"><span> # 34</span> ago | Junho 2014</p>
+					</li>
+					<li><img src="images/capa-revista.jpg" width="160px" height="220px">
+						<h3>Nossas Notícias</h3><p class="text"><span> # 35</span> dez 2013</p>
+					</li>
+				</ul>
+			</section>		
+		</div>
+	</section>
+
+<?php include('includes/footer.php'); ?>
